dbd4ac901cb27f7a876a3a59a32781eefd883f3b
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2022-10-02 17:26:40 -0400

    Create atomicsearch.js

---
+++ kjEncrypted/atomicsearch.js
@@ -0,0 +1,3 @@
+var atomicSearchWidgetScript = document.createElement("script");
+atomicSearchWidgetScript.src = "https://js.atomicsearchwidget.com/atomic_search_widget.js";
+document.getElementsByTagName("head")[0].appendChild(atomicSearchWidgetScript);
