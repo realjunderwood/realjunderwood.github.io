5dfe74d1c4a1054f01f0bd1752f0e87bd61d5e28
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-02 18:13:30 -0500

    facemusic

--- facemusic/spotify_auth.js
+++ facemusic/spotify_auth.js
@@ -2,11 +2,6 @@ console.log("spotify_auth.js loaded");
 
 
 
-const clientId = 'b5ac45b92b3243668cdb8390ab51a04d';
-const redirectUri = 'https://www.jamesunderwood.net/facemusic';
-
-const scope = 'user-read-private user-read-email';
-const authUrl = new URL("https://accounts.spotify.com/authorize")
 
 
 // 
@@ -55,13 +50,37 @@ async function generateCodeChallenge(length) {
       window.location.href = authUrl.toString()
 
 
-      //const urlParams = new URLSearchParams(window.location.search);
-      //console.log("uhhhhhh")
+      const urlParams = new URLSearchParams(window.location.search);
+      if (urlParams.get('error')) {
+        console.log("an error was returned");
+      }
+      let code = urlParams.get('code');
 
 
 
 }
 
 
+const getToken = async code => {
 
+    let codeVerifier = localStorage.getItem('code_verifier');
+
+    const payload = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            client_id: clientId,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        }),
+    }
+
+    const body = await fetch(url, payload);
+    const response =await body.json();
+    localStorage.setItem('access_token', response.access_token);
+}
 
