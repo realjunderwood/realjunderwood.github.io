cfbe89ecdf58053814bca5322a5efc1e45582b99
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-04 03:46:13 -0500

    facemusic

--- facemusic/spotify_auth.js
+++ facemusic/spotify_auth.js
@@ -153,16 +153,23 @@ async function getTopTracks(accessToken,offsetVal) {
     });
   
     const data = await response.json();
+
+
+
+    getProfile(accessToken);
+
     return(data.audio_features);
+
+
+
+
+
+
 
   }
 
 
-  async function createPlaylist(howFeeling) {
- 
-
-
-
+  async function createPlaylist(accessToken,howFeeling,userID) {
 
     const payload = {
         method: 'POST',
@@ -187,4 +194,20 @@ async function getTopTracks(accessToken,offsetVal) {
 
 
   
+  }
+
+
+
+  async function getProfile(accessToken) {
+    let accessToken = localStorage.getItem('access_token');
+  
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
+    });
+  
+    const data = await response.json();
+    console.log("profile");
+    console.log(data);
   }
