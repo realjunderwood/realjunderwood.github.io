d63fc7f224788abf0b6008df81b34d0e1f8103d0
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-04 23:01:46 -0500

    facemusic

---
+++ facemusic/react_step2point2.js
@@ -0,0 +1,82 @@
+const Reactstep2point1 = () => {
+    return (
+
+<div id="step2point2">
+    <div id="songRecContainer">
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+            
+        </div>
+        <div className="songrec">
+            <p></p>
+            <p></p>
+        </div>
+
+ 
+
+    </div>
+    <button className="roundButton" onClick={createPlaylistParent()}>Click to add playlist to Spotify</button>
+    <p><small><i>Don't worry: the playlist will be private by default</i></small></p>
+</div>
+
+    )
+};
