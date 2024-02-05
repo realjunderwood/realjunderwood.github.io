bcbdfd347fe8a37a5d86d26754a6733434c8d3cb
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-04 23:01:23 -0500

    facemusic

--- facemusic/react_step2.js
+++ facemusic/react_step2.js
@@ -4,20 +4,8 @@ const Reactstep2 = () => {
 <div id="step2">
 <p id="sucesssaccess">Successfully accessed Spotify.</p>
 <button className="roundButton" onClick={startVideo} id="webcamButton">Click to enable webcam</button>
-<div id="step2point1">
-    <div id="videoPlusSnapButton">
-        <div id="videoContainer">
-            <video autoPlay="true" id="videoElement">
-            </video>
-        </div>
-        <button id="snappicButton" onClick={takepicParent}><h3 style={{display:inline-block}}>snap picture</h3><img src="camsvg.svg" style="margin-left:10px;vertical-align:middle;" width="30px" /></button>
-    </div>
-    <div id="canvasPlusHappiness">
-    <canvas id="canvas"></canvas>
-    <div id="howyourelooking">
-        <p>You're looking  <span id="happiness"></span></p>
-    </div>
-</div>
+
+<Reactstep2point1 />
 
 <div id="step2point2">
     <div id="songRecContainer">
@@ -98,6 +86,6 @@ const Reactstep2 = () => {
     
 
 </div>
-</div>
+
     );
 };
