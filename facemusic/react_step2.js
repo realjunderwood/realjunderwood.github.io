eb5c51ace0737b58e7a90e4c895ca6971062252b
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-04 23:50:33 -0500

    facemusic

--- facemusic/react_step2.js
+++ facemusic/react_step2.js
@@ -2,6 +2,7 @@ const Reactstep2 = () => {
     return (
 
 <div id="step2">
+    <p>uhh hey</p>
 <p id="sucesssaccess">Successfully accessed Spotify.</p>
 
 {curState == 1 && <button className="roundButton" onClick={startVideo} id="webcamButton">Click to enable webcam</button>}
