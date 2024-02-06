38cce63a1aafea30395ecf8edb2ad2ca8ce02f34
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-05 21:08:29 -0500

    facemusic

--- facemusic/react_step2point1.js
+++ facemusic/react_step2point1.js
@@ -185,7 +185,7 @@ async function getMusic(accessToken) {
     <div id="canvasPlusHappiness">
     {theCanvas}
     <div id="howyourelooking">
-        <p>You're looking  <span id="happiness">{happinessCalcedWord}</span>}</p>
+        <p>You're looking  <span id="happiness">{happinessCalcedWord}</span></p>
     </div>
     <p>hidden image</p>
     {myImg}
