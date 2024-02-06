395d09e92b9bee30645e92829b7cc16f86e7b966
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-06 17:26:13 -0500

    facemusic

--- facemusic/react_step2.js
+++ facemusic/react_step2.js
@@ -182,6 +182,7 @@ const Reactstep2 = ( {curState, setCurState }) => {
 <p>Is it true? {tracksPoolDone === true}</p>
 { ( (curState == 8 || curState == 7) ) && <Reactstep2point1 curState={curState} setCurState={setCurState}  tracksPool={tracksPoolReal} />  }
 <p>{tracksPoolReal}</p>
+<p>p above is trrakspollroeal</p>
 
 {curState == 6 && <Reactstep2point2 curState={curState} setCurState={setCurState} /> }
 
