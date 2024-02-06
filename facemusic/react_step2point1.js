5c3e5cb30ed9e713a2c8a2fb4baf4541e13e897f
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-05 21:09:29 -0500

    facemusic

--- facemusic/react_step2point1.js
+++ facemusic/react_step2point1.js
@@ -135,14 +135,14 @@ async function getMusic(accessToken) {
     const terms = ["short_term","medium_term","long_term"];
 
 
-    for (i = 0; i < 2; i++) { // Loop through twice: once to get top 50 songs, then again to get next 50 songs
+    for (let i = 0; i < 2; i++) { // Loop through twice: once to get top 50 songs, then again to get next 50 songs
        // await getTopArtists(accessToken,49*i).then((value) => {
             // To implement: add to tracksPool top 5 songs of all the top artists
         //});
 
-        for (k=0;k<terms.length;k++) { // Loop through short term, medium term, and long term top songs
+        for (let k=0;k<terms.length;k++) { // Loop through short term, medium term, and long term top songs
             await getTopTracks(accessToken,49*i,terms[k]).then((value) => {
-                for (j=0;j<50;j++) {
+                for (let j=0;j<50;j++) {
                     const alreadyInTracksPool = tracksPool.some(el => el.id === value[j].id || (el.artists[0].name === value[j].artists[0].name && el.name === value[j].name)); //Make sure we don't have duplicate songs. Duplicates have same track ID, or both same name and same artist
                     if (!alreadyInTracksPool) {
                         tracksPool.push(value[j]);
