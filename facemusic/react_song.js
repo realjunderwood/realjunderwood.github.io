62e01dbcf6d1f4e08b53cf1046f5108f39f5ab30
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-07 02:39:41 -0500

    facemusic

--- facemusic/react_song.js
+++ facemusic/react_song.js
@@ -8,6 +8,7 @@ function Song({ title, artist, albumCoverUrl, keyy }) {
     <div className="songrec" key={keyy}>
       <img className="songrecimg" src={albumCoverUrl} alt="Album Cover" />
       <h5 className="songrectitle">{title}</h5>
+      <h6 classname="songrecartist">{artist}</h6>
 
     </div>
   );
@@ -20,6 +21,7 @@ function Song({ title, artist, albumCoverUrl, keyy }) {
         <Song
           title={song.name}
           albumCoverUrl={song.album.images[0].url}
+          artist={song.artists[0].name}
           keyy={song.id}
         />
       ))}
