3cf12b3bbd3ba6cb93695c036556417fe4f6fb72
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-07 15:52:55 -0500

    facemusic

--- facemusic/react_song.js
+++ facemusic/react_song.js
@@ -3,11 +3,11 @@
 const Reactsong = ({ myTracksPool }) => {
 
 
-function Song({ title, artist, albumCoverUrl, keyy }) {
+function Song({ title, artist, albumCoverUrl, keyy, extUrl }) {
   return (
     <div className="songrec" key={keyy}>
       <img className="songrecimg" src={albumCoverUrl} alt="Album Cover" />
-      <h5 className="songrectitle">{title}</h5>
+      <h5 className="songrectitle"><a href="{extUrl}">{title}</a></h5>
       <h6 classname="songrecartist">{artist}</h6>
 
     </div>
@@ -23,6 +23,7 @@ function Song({ title, artist, albumCoverUrl, keyy }) {
           albumCoverUrl={song.album.images[0].url}
           artist={song.artists[0].name}
           keyy={song.id}
+          extUrl={song.external_urls.spotify}
         />
       ))}
     </div>
