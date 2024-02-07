8b358fe9dbcb6171115b21881055ee55a5abd54e
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-06 20:47:21 -0500

    facemusic

--- facemusic/react_song.js
+++ facemusic/react_song.js
@@ -6,8 +6,8 @@ const Reactsong = ({ myTracksPool }) => {
 function Song({ title, artist, albumCoverUrl, keyy }) {
   return (
     <div className="songrec" key={keyy}>
-      <img class="songrecimg" src={albumCoverUrl} alt="Album Cover" />
-      <h5 class="songrectitle">{title}</h5>
+      <img className="songrecimg" src={albumCoverUrl} alt="Album Cover" />
+      <h5 className="songrectitle">{title}</h5>
 
     </div>
   );
