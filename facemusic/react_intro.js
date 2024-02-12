88927f724000e00949a6bafdc305aa360b56481f
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-12 13:57:46 -0500

    Update react_intro.js

--- facemusic/react_intro.js
+++ facemusic/react_intro.js
@@ -1,50 +1,35 @@
 const Reactintro = () => {
     return (
 
-        <div id="intro">
+    <div id="intro">
 
-    <div className="introTextDiv" style={{marginTop:30+'vh'}}>
-        <h1>We like to listen to music that matches our mood.</h1>
+        <div className="introTextDiv" style={{marginTop:30+'vh'}}>
+            <h1>We like to listen to music that matches our mood.</h1>
+        </div>
+        <div className="introTextDiv" style={{marginTop:3+'vh'}}>
+            <h3>Scroll to continue ↓</h3>
+        </div>
+        
+        <div className="introTextDiv" style={{marginTop:55+'vh'}}>
+            <h1>But it's hard to remember all the songs you like, and how they sound.</h1>
+        </div>
+        <div className="introTextDiv" style={{marginTop:5+'vh'}}>
+            <h1>Plus, figuring out whether you are happy or sad requires a lot of emotional intelligence.</h1>
+        </div>
+
+        <div className="introTextDiv" style={{marginTop:55+'vh'}}>
+            <h1>FaceMusic solves this problem.</h1>
+        </div>
+        <div className="introTextDiv" style={{marginTop:5+'vh'}}>
+            <h1>First, we take a picture of your face to determine your mood.</h1>
+        </div>
+        <div className="introTextDiv" style={{marginTop:5+'vh'}}>
+            <h1>Then, pulling from songs you like, we make a playlist that matches your detected mood.</h1>
+        </div>
+        <div className="introTextDiv" style={{marginTop:5+'vh',marginBottom:20+'vh'}}>
+            <h4>Keep going to try it out!</h4>
+        </div>
     </div>
-    <div className="introTextDiv" style={{marginTop:3+'vh'}}>
-        <h3>Scroll to continue ↓</h3>
-    </div>
-    {/* <!--<div class="introTextDiv" style="margin-top:100vh;">
-        <h1>But there's a problem:</h1>
-    </div>--> */}
-    <div className="introTextDiv" style={{marginTop:55+'vh'}}>
-        <h1>But it's hard to remember all the songs you like, and how they sound.</h1>
-    </div>
-
-    <div className="introTextDiv" style={{marginTop:5+'vh'}}>
-        <h1>Plus, figuring out whether you are happy or sad requires a lot of emotional intelligence.</h1>
-
-    </div>
-
-    <div className="introTextDiv" style={{marginTop:55+'vh'}}>
-        <h1>FaceMusic solves this problem.</h1>
-    </div>
-
-    <div className="introTextDiv" style={{marginTop:5+'vh'}}>
-        <h1>First, we take a picture of your face to determine your mood.</h1>
-    </div>
-
-    <div className="introTextDiv" style={{marginTop:5+'vh'}}>
-        {/* margin-bottom:30vh; */}
-        <h1>Then, pulling from songs you like, we make a playlist that matches your detected mood.</h1>
-    </div>
-
-    <div className="introTextDiv" style={{marginTop:5+'vh',marginBottom:20+'vh'}}>
-        {/* margin-bottom:30vh; */}
-        <h4>Keep going to try it out!</h4>
-    </div>
-
-
-</div>
-
-
-
 
     );
   };
-  
