9ad77de163899a71ad7b2663229ebd302c94e7e7
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-12 14:03:03 -0500

    Update react_song.js

--- facemusic/react_song.js
+++ facemusic/react_song.js
@@ -2,34 +2,30 @@
 
 const Reactsong = ({ myTracksPool }) => {
 
-
-function Song({ title, artist, albumCoverUrl, keyy, extUrl }) {
-  return (
-    <div className="songrec" key={keyy}>
-      <img className="songrecimg" src={albumCoverUrl} alt="Album Cover" />
-      <h5 className="songrectitle"><a href={extUrl}>{title}</a></h5>
-      <h6 classname="songrecartist">{artist}</h6>
-
-    </div>
-  );
-}
+  function Song({ title, artist, albumCoverUrl, keyy, extUrl }) {
+    return (
+      <div className="songrec" key={keyy}>
+        <img className="songrecimg" src={albumCoverUrl} alt="Album Cover" />
+        <h5 className="songrectitle"><a href={extUrl}>{title}</a></h5>
+        <h6 classname="songrecartist">{artist}</h6>
+      </div>
+    );
+  }
 
   return (
     <div id="marqueecontainer">
-    <div id="marquee">
-      {myTracksPool.map(song => (
-        <Song
-          title={song.name}
-          albumCoverUrl={song.album.images[0].url}
-          artist={song.artists[0].name}
-          keyy={song.id}
-          extUrl={song.external_urls.spotify}
-        />
-      ))}
-    </div>
+      <div id="marquee">
+        {myTracksPool.map(song => (
+          <Song
+            title={song.name}
+            albumCoverUrl={song.album.images[0].url}
+            artist={song.artists[0].name}
+            keyy={song.id}
+            extUrl={song.external_urls.spotify}
+          />
+        ))}
+      </div>
     </div>
   );
-
-
 
 }
