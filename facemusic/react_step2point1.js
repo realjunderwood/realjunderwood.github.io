671ded686e09dce3c4d273e3340ea5fbbc1d097b
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-13 18:32:49 -0500

    Update react_step2point1.js

--- facemusic/react_step2point1.js
+++ facemusic/react_step2point1.js
@@ -244,10 +244,7 @@ const Reactstep2point1 = ({ curState, setCurState, tracksPool }) => {
           </div>
           <hr />
           <p>
-            <b>How it works</b>
-          </p>
-          <p>
-           <a href="https://github.com/realjunderwood/realjunderwood.github.io/blob/main/facemusic/README.md" target="_BLANK">How it works</a>
+           <b><a href="https://github.com/realjunderwood/realjunderwood.github.io/blob/main/facemusic/README.md" target="_BLANK">How it works</a><b>
           </p>
         </div>
       )}
