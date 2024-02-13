76fe5f2c72697a92fd1b158f926556f4908292c8
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2024-02-13 13:54:44 -0500

    Update react_step1.js

--- facemusic/react_step1.js
+++ facemusic/react_step1.js
@@ -2,10 +2,9 @@
 
 const Reactstep1 = ( { curState, setCurState } ) => {
     async function connectSpot() {
-
         generateCodeChallenge(); // Function in react_spotify_auth.js; takes user to Spotify.
-
     }
+    
     return (
         <div id="step1">
             <button id="connectspotButton" className="roundButton" onClick={connectSpot}>Connect to Spotify to get started</button>
